e0f7a7778ba05c6f416cfb9c9d23fda585de9afc
Devin Amlen <devinamlen@gmail.com>
2023-08-10 12:22:17 -0400

    Now Saves last search and displays it on page load

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ const keys = {
     youtube: "AIzaSyBK5m152cl5khgr0pnjdjJ8xebqD2Jo8fc"
 }
 
+var storageID = 'SONGSTORIES_lastSearch';
+
+var lastSearch = undefined;
 
 var videoPlayer = $('#video-player');
 
@@ -17,7 +20,10 @@ var Get = function(url, onSuccess, onFail){
         console.log("Response recieved! Status code: "+response.status);
         if(response.status === 200){
             var asJson = response.json();
+            console.log(asJson);
+            
             return asJson;
+            
         }
         else{
             onFail(response, null);
@@ -71,36 +77,60 @@ function onPlayerReady(event) {
     player.stopVideo();
   }
 
-var ButtonClick = function(event){
-    event.preventDefault();
-
-    var search = searchBar.val();
+function ErrorHandling(response, asJson){
     videoPlayer.addClass('u-display-none');
-    Get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q="+search+"&type=video&key="+keys.youtube+"", 
-    function(data){
-        var result = data.items[0];
-        if(result !== null && result !== undefined){
-            videoPlayer.attr('src','https://youtube.com/embed/'+result.id.videoId+"?origin=*");
-            $(videoPlayer).removeClass('u-display-none');
-        }
-    })
-    Get("https://en.wikipedia.org/w/api.php?&origin=*&action=parse&page="+search+"&format=json&redirects", function(data){
-        FillWikiDisplay(data.parse.text["*"]);
-    },
-    function(response, asJson){
-        
-        wikiDisplay.empty();
-        var holder = $('<p>');
-        var errorMessage = 'Error status '+response.status+'';
-        if(asJson !== null && asJson !== undefined){
+    
+    wikiDisplay.empty();
+    var holder = $('<p>');
+    var errorMessage = 'Error status '+response.status+'';
+    if(asJson !== null && asJson !== undefined){
+        if(asJson.error !== null && asJson.error !== undefined){
             errorMessage += ("Code: "+asJson.error.code + "; Info: "+asJson.error.info); 
         }
-        holder.text(errorMessage);
-        wikiDisplay.append(holder);
+    }
+    holder.text(errorMessage);
+    wikiDisplay.append(holder);
+}
+
+var Search = function(search){
+    Get("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q="+search+"&type=video&key="+keys.youtube+"", 
+    function(data){
+        if(data !== null && data !== undefined){
+            if(data.items !== null && data.items !== undefined){
+                var result = data.items[0];
+                if(result !== null && result !== undefined){
+                    videoPlayer.attr('src','https://youtube.com/embed/'+result.id.videoId+"?origin=*");
+                    $(videoPlayer).removeClass('u-display-none');
+                }
+            }
+        }
+    }, function(response, asJson){
+        ErrorHandling(response, asJson);
+    })
+    Get("https://en.wikipedia.org/w/api.php?&origin=*&action=parse&page="+search+"&format=json&redirects", function(data){
+        if(data !== undefined && data !== null){
+
+            if(data.parse !== undefined && data.parse !== null){
+                FillWikiDisplay(data.parse.text["*"]);
+                localStorage.setItem(storageID, search);
+            }
+        }
+    },
+    function(response, asJson){
+        ErrorHandling(response, asJson);
     });
 }
 
+var ButtonClick = function(event){
+    event.preventDefault();
+    var search = searchBar.val();
+    videoPlayer.addClass('u-display-none');
+    Search(search);
+}
+
+
 searchBtn.on('click', ButtonClick);
 
-
-var keyword = "Sting";
+if(localStorage.getItem(storageID) !== null && localStorage.getItem(storageID) !== undefined){
+    Search(localStorage.getItem(storageID));
+}
